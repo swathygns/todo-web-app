d8154e0265317d01663dda4c6541006291dbcf7f
swathygns <swathygns@gmail.com>
2022-12-25 23:47:07 -0500

    rename files from jsx to tsx

--- src/component/TodoItem.jsx
+++
@@ -1,9 +0,0 @@
-export const TodoItem = (props) => {
-    return (
-        <li>
-            <input type= {"checkbox"} />
-            <label> {props?.props?.name} </label>
-            <label> {props?.props?.status} </label>
-        </li>
-    )
-}
