9849e6644b0acdb67e1430f610574de42df3f57e
swathygns <swathygns@gmail.com>
2022-12-26 23:49:17 -0500

    integrate with flask api

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { TodoItem } from './component/TodoItem';
-const items = [
-  {
-    "name":"Buy Milk",
-    "status":"Complete",
-    "id":1
-  },
-  {
-    "name":"phone bill",
-    "status":"active",
-    "id":2
-  }
-]
+
 function App() {
+  let [todoItem,setTodoItem] = useState([])
+  useEffect( () => {
+
+     fetch('http://localhost:8080/todoItems/')
+    .then((response) => response.json())
+    .then((data) => setTodoItem(data));
+
+  }
+
+  )
   return (
     <div className="App">
       <p>Welcome!!</p>
       <div>
         <ul>
           {
-            items.map((item) => {
+            todoItem.map((item) => {
               return (< TodoItem props = {item} />) 
             })
           }
